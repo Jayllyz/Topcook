e2b147b08c065f24e22fbf8243afeffcd2c9335e
Danette10 <dan.sebag1007@gmail.com>
2022-02-23 16:17:47 +0100

    modif button searchbar

--- includes/header.php
+++ includes/header.php
@@ -32,7 +32,7 @@
   <div class="container">
     <form class="d-flex searchbar pt-3 pb-5">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
+          <button class="btn btn-search btn-outline-success" type="submit">Search</button>
     </form>
   </div>
   </header>

115ef6553699c5929011ba9334dc367dfc126961
Antony David <antonydavid945@gmail.com>
2022-03-08 08:58:10 +0100

    includes head

--- index.php
+++ index.php
@@ -1,15 +1,11 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/svg+xml" sizes="16x16" href="images/topcook_logo.svg">
-    <link rel="stylesheet" href="css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>TopCook</title>
-</head>
+<?php
+$title = "TopCook - Accueil";
+include "includes/head.php";
+?>
 <body>
-    <?php include('includes/header.php'); ?>
+    <?php include "includes/header.php"; ?>
     
     <main>
       <h1 class="pb-3 text-center"><strong>La recette du moment</strong></h1>
@@ -168,7 +164,7 @@
       </div>
     </main>
 
-    <?php include('includes/footer.php'); ?>
+    <?php include "includes/footer.php"; ?>
 
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>

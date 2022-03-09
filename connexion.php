15b6838e40de4e1cb8d64eabdecfbe55ad96b0a9
Jayllyz <antonydavid945@gmail.com>
2022-03-09 09:49:18 +0100

    + connexion & inscription php

---
+++ connexion.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php
+$title = "Connexion";
+include "includes/head.php";
+?>
+<body>
+    <?php include "includes/header.php"; ?>
+    <main>
+        
+    </main>
+    <?php include "includes/footer.php"; ?>
+</body>
+</html>
---
+++ inscription.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php
+$title = "Inscription";
+include "includes/head.php";
+?>
+<body>
+    <?php include "includes/header.php"; ?>
+    <main>
+        
+    </main>
+    <?php include "includes/footer.php"; ?>
+</body>
+</html>

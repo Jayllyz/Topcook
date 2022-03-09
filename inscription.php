244a331fe7bd0aeebc6a143637a5a8476c4455b6
Danette10 <dan.sebag1007@gmail.com>
2022-03-09 16:11:46 +0100

    Verification inscription

--- inscription.php
+++ inscription.php
@@ -7,7 +7,39 @@ include "includes/head.php";
 <body>
     <?php include "includes/header.php"; ?>
     <main>
-        
+            <form class="container col-4" action="verifications/verification_inscription.php" method="post" enctype="multipart/form-data">
+                <div class="mb-3">
+                    <label class="form-label">Pseudo</label>
+                    <input type="text" name="pseudo" class="form-control" required>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Adresse mail</label>
+                    <input type="email" name="email" class="form-control" required>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Mot de passe</label>
+                    <input type="password" name="password" class="form-control" required>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Confirmation du mot de passe</label>
+                    <input type="password" name="conf_password" class="form-control" required>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Photos de profil</label>
+                    <input type="file" name="image" class="form-control" accept="image/png, image/jpeg">
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">Votre date de naissance</label>
+                    <input type="date" name="birth" class="form-control" accept="image/png, image/jpeg">
+                </div>
+                <div class="mb-3 form-check">
+                    
+                    <label class="form-check-label">Se souvenir de moi
+                        <input type="checkbox" class="form-check-input">
+                    </label>
+                </div>
+            <button type="submit" class="btn">Submit</button>
+        </form>
     </main>
     <?php include "includes/footer.php"; ?>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
---
+++ verifications/verification_inscription.php
@@ -0,0 +1,19 @@
+<?php
+    include ('includes/db.php');
+    $email = $_POST['email'];
+    $pseudo = $_POST['pseudo'];
+    $password = $_POST['password'];
+    $conf_password = $_POST['conf_password'];
+    $birth = $_POST['birth'];
+    
+
+    if (isset($email) && isset($pseudo) && isset($password) && isset($conf_password) && !empty($email) && !empty($pseudo) && !empty($password) && !empty($conf_password)) {
+        $req = $db->prepare("INSERT INTO user (pseudo,email,password,date_birth) VALUES (:pseudo,:password,:date_birth)");
+        $req->execute([
+            'pseudo' => $pseudo,
+            'email' => $email,
+            'password' => $password,
+            'date_birth' => $birth
+        ]);
+    }
+?>

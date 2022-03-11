edf803e2cb831749f5097fcb946367e9c7eef707
Danette10 <dan.sebag1007@gmail.com>
2022-03-11 23:09:59 +0100

    Update dark mode

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,4 +1,14 @@
 function darkMode() {
   var element = document.body;
-  element.classList.toggle("dark-mode"); // Test d'exemple pour dark-mode (à modifier selon nos couleurs)
+  element.style.background = "black"; // Test d'exemple pour dark-mode (à modifier selon nos couleurs)
+  element.style.color = "white";
+  document.getElementById("sun").style.display = "block";
+  document.getElementById("moon").style.display = "none";
+}
+function lightMode() {
+  var element_sun = document.body;
+  element_sun.style.background = "white"; // Test d'exemple pour dark-mode (à modifier selon nos couleurs)
+  element_sun.style.color = "black";
+  document.getElementById("moon").style.display = "block";
+  document.getElementById("sun").style.display = "none";
 }

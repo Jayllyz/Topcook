2af1e60c0a0abbc99099ca51139ad30128c9ecbf
Danette10 <dan.sebag1007@gmail.com>
2022-03-12 00:24:54 +0100

    Update dark-mode color

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,8 @@
-<header>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<header id="header">
+      <nav class="navbar navbar-expand-lg navbar-light" id="navbar">
         <div class="container-fluid">
           <a class="navbar-brand" href="index.php"><img src="../images/topcook_logo.svg" width="50" height="50"></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <button class="navbar-toggler" id="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
@@ -35,7 +35,7 @@
 
             <div id="sun">
               <button type="button" class="btn sun" onClick="lightMode()">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sun-fill" viewBox="0 0 16 16">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sun-fill" viewBox="0 0 16 16" id="svg_sun">
                   <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
                 </svg>
               </button>
--- js/darkMode.js
+++ js/darkMode.js
@@ -1,14 +1,18 @@
 function darkMode() {
   var element = document.body;
-  element.style.background = "black"; // Test d'exemple pour dark-mode (à modifier selon nos couleurs)
-  element.style.color = "white";
   document.getElementById("sun").style.display = "block";
   document.getElementById("moon").style.display = "none";
+  element.style.background = "#011A27"; // Test d'exemple pour dark-mode (à modifier selon nos couleurs)
+  document.getElementById("navbar").style.background = "#063C58";
+  document.getElementById("svg_sun").style.color = "white";
+  document.getElementById("navbar-toggler").style.background = "white";
+  document.getElementById("form").style.color = "white";
 }
 function lightMode() {
   var element_sun = document.body;
-  element_sun.style.background = "white"; // Test d'exemple pour dark-mode (à modifier selon nos couleurs)
-  element_sun.style.color = "black";
   document.getElementById("moon").style.display = "block";
   document.getElementById("sun").style.display = "none";
+  element_sun.style.background = "white"; // Test d'exemple pour dark-mode (à modifier selon nos couleurs)
+  element_sun.style.color = "black";
+  document.getElementById("navbar").style.background = "#f8f9fa";
 }

9615fda80f14dfbaf618993158ac84546e4f779d
Antony David <antonydavid945@gmail.com>
2022-03-12 15:36:25 +0100

    Update verification_connexion.php

--- verifications/verification_connexion.php
+++ verifications/verification_connexion.php
@@ -35,12 +35,6 @@ if (isset($_POST["submit"])) {
   ]);
   $reponse = $req->fetchAll(PDO::FETCH_ASSOC);
   if (count($reponse) == 0) {
-    $selectID = $db->prepare("SELECT id FROM USER WHERE email = :email");
-    $selectID->execute([
-      "email" => $_POST["login"],
-    ]);
-    $result = $selectID->fetchAll();
-
     foreach ($result as $id) {
       $_SESSION["id"] = $id["id"];
     }

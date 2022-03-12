e9ae0b96a390f7db7c2bfac297554b636d3b04fb
Antony David <antonydavid945@gmail.com>
2022-03-12 16:10:14 +0100

    Update mailer.php

--- includes/mailer.php
+++ includes/mailer.php
@@ -5,24 +5,21 @@ require "../PHPMailer/src/Exception.php";
 
 $mail = new PHPMailer();
 $mail->isSMTP();
-$mail->Host = 'smtp.gmail.com';
+$mail->Host = "smtp.gmail.com";
 $mail->Port = 587;
 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
 $mail->SMTPAuth = true;
-$mail->Username = 'topcook2022@gmail.com';
-$mail->Password = 'TopCook.2022#ESGI';
-$mail->setFrom('dsebagpro@gmail.com', 'No-Reply');
+$mail->Username = "topcook2022@gmail.com";
+$mail->Password = "TopCook.2022#ESGI";
+$mail->setFrom("dsebagpro@gmail.com", "No-Reply");
 $mail->addAddress($email);
-$mail->Subject = 'Confirm your registration';
-$mail->Message = 'Valid your registration !';
-$mail->msgHTML(
-
-  '<h1>SOMBIE LE BOSS</h1>'
-);
+$mail->Subject = "Confirm your registration";
+$mail->Message = "Valid your registration !";
+$mail->msgHTML("<h1>SOMBIE LE BOSS</h1>");
 if (!$mail->send()) {
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
+  echo "Mailer Error: " . $mail->ErrorInfo;
 } else {
-    echo "
+  echo "
     <div class='mx-auto text-center'>
     <div class='alert alert-success' role='alert'>
       <h4 class='alert-heading display-5'>Well done !</h4>
@@ -30,13 +27,18 @@ if (!$mail->send()) {
       have received a confirmation email. <br>Check your spam if we are not in your inbox.</p>
       <hr>
       <p class='mb-0 fs-3'>Click the link in the email to start enjoying our site fully.</p>
-      <p class='mb-0 fs-3'>You can close this page. If you did not receive the email, click <a href='https://dna-esgi.fr/includes/email_resend.php?email=".$email."&token=".$token."&id_account=".$id_account."'>here</a> to resend it.
+      <p class='mb-0 fs-3'>You can close this page. If you did not receive the email, click <a href='https://dna-esgi.fr/includes/email_resend.php?email=" .
+    $email .
+    "&token=" .
+    $token .
+    "&id_account=" .
+    $id_account .
+    "'>here</a> to resend it.
       </div>
       </div>
     </div>
     </div>
     ";
 }
-message.txt
-3 KB
+
 ?>

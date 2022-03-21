bb116f91f223f0ad9332d9ccd46adeacf78e4adc
Jayllyz <antonydavid945@gmail.com>
2022-03-21 16:52:19 +0100

    Update banned.php

--- profile/banned.php
+++ profile/banned.php
@@ -1,14 +1,8 @@
 <?php session_start(); ?>
-
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+    <?php include "../includes/head.php"; ?>
 <body>
-    <?php var_dump($_SESSION["rights"]); ?>
+    <h1>BANNED</h1>
 </body>
 </html>

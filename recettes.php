8e882528fe39861add07b11100a5faab53b81b73
Jayllyz <antonydavid945@gmail.com>
2022-04-01 12:26:56 +0200

    update btn form

--- recettes.php
+++ recettes.php
@@ -147,7 +147,7 @@ if(isset($_SESSION["id"])) {
                     <label class="form-label">Type de recette</label>
                     <input type="text" name="type" class="form-control" required>
                 
-                    <button type="submit" class="btn btn-secondary" data-bs-dismiss="modal">Envoyer</button>
+                    <button type="submit" name="submit" class="btn" data-bs-dismiss="modal">Envoyer</button>
 
                     </div>
                 </form>

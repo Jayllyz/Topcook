6f4163fd6c3ab5454262709bb3973c776610acfa
Jayllyz <antonydavid945@gmail.com>
2022-04-02 21:55:42 +0200

    Update mesRecettes.php

--- profile/mesRecettes.php
+++ profile/mesRecettes.php
@@ -1,5 +1,6 @@
 <?php session_start();
 include "../includes/db.php";
+
 if (isset($_SESSION["id"])) { ?>
 
 <!DOCTYPE html>
@@ -12,11 +13,29 @@ include "../includes/head.php";
 ?>
 <body>
     <?php include "../includes/header.php"; ?>    
+    <main>
+    <h1 class="pb-3 text-center"><strong>Mes recettes<strong></h1>
 
+      <?php
+          $query = $db->prepare(
+              "SELECT name, images FROM RECIPE WHERE id_user = :id_user ORDER BY id DESC"
+          );
+          $query->execute([
+              "id_user" => $_SESSION["id"]
+          ]);
+          $result = $query->fetchAll(PDO::FETCH_ASSOC);
+      foreach ($result as $select) { ?>
 
-
-
-
+      <div class="container g-1" id="recettes">
+          <div class="pb-4 row justify-content-md-center">
+              <div class=" col col-md-3">
+                  <?= '<img src="../uploads/recipe/' . $select["images"] . '" class="rounded img-fluid" alt="image -' . $select['names'] . '">'; ?>
+                  <h4 class="text-center"><?= $select['name'] ?></h4>
+              </div>
+      </div>
+      
+      <?php } ?>
+    </main>
     <?php include "../includes/footer.php"; ?>
     <script src="../js/app.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>

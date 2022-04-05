80442495c7f25e9823c096358678b3594ed470aa
Anthony Jabre <47538252+silvertstart@users.noreply.github.com>
2022-04-05 18:12:25 +0200

    Add files via upload

---
+++ publication.php
@@ -0,0 +1,33 @@
+<?php
+require('actions/questions/publicationaction.php');
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<?php include 'includes/head.php'; ?>
+<body>
+  <?php include 'includes/header.php'; ?>
+
+  <br><br>
+  <div class="container">
+  <?php
+
+    while($question = $getAllMyQuestions->fetch()){
+      ?>
+      <div class="card">
+        <div class="card-header">
+          <?php echo $question['titre'] ?>
+        </div>
+        <div class="card-body">
+          <p class="card-text"><?php echo $question['description'] ?></p>
+          <a href="#" class="btn btn-primary">accéder a la publication</a>
+       </div>
+      </div>
+      <br><br>
+      <?php
+    }
+
+   ?>
+ </div>
+</body>
+<?php include "includes/footer.php";?>
+</html>
---
+++ thread.php
@@ -0,0 +1,39 @@
+<?php
+  require('actions/questions/creerthread.php');
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<?php include "includes/head.php";?>
+<body>
+  <?php include 'includes/header.php';?>
+  <br><br>
+  <form class="container" method="POST">
+
+    <?php
+      if(isset($errorMsg))
+      {
+        echo '<p>' . $errorMsg . '<p>';
+      }elseif (isset($successMsg)) {
+        echo '<p>' . $successMsg . '</p>';
+      }
+     ?>
+
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Titre</label>
+      <input type="text" class="form-control" name="title">
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Description</label>
+      <textarea class="form-control" name="description"></textarea>
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Contenu</label>
+      <textarea class="form-control" name="contenu"></textarea>
+    </div>
+    <button type="submit" class="btn btn-primary" name="bouton">Publier</button>
+    <br><br>
+    <a href="publication.php"><p>Voir les publications</p></a>
+  </form>
+</body>
+<?php include "includes/footer.php"; ?>
+</html>

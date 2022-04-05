4430ccf67fc7dd77dd84646727ddc55ae6849db6
Anthony Jabre <47538252+silvertstart@users.noreply.github.com>
2022-04-05 18:46:17 +0200

    Update publicationaction.php

--- questions/publicationaction.php
+++ questions/publicationaction.php
@@ -1,6 +1,6 @@
 <?php
   session_start();
-  include "../includes/db.php";
+  include "includes/db.php";
 
   $getAllMyQuestions = $bdd->prepare('SELECT id, titre, description FROM questions WHERE id_auteur = ?');
   $getAllMyQuestions->execute(array($_SESSION['id']));

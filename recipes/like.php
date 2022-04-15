82bdf2a3916abce92045bdd39f59e8ad742d5b23
Jayllyz <antonydavid945@gmail.com>
2022-04-15 10:49:25 +0200

    likes logs

--- recipes/like.php
+++ recipes/like.php
@@ -16,7 +16,15 @@ if (isset($_SESSION["id"])) {
     "id_recipe" => $id_recipe,
     "id_user" => $_SESSION["id"],
   ]);
-
+  $date = date("d/m/Y H:i:s");
+  $log_recipe = fopen("../log/recipe_likes/$name.txt", "a+");
+  fputs($log_recipe, $name . " ");
+  fputs($log_recipe, "liké le ");
+  fputs($log_recipe, $date);
+  fputs($log_recipe, "par ");
+  fputs($log_recipe, $_SESSION["id"]);
+  fputs($log_recipe, "\n");
+  fclose($log_recipe);
   header(
     "location: https://topcook.site/recipes/recipe.php?name=$name&id=$id_recipe&message=Votre vote a bien été pris en compte !&type=success"
   );

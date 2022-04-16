f2b210914312ea8bd2cb06689d634e56b1a68feb
Danette10 <dan.sebag1007@gmail.com>
2022-04-16 16:05:41 +0200

    update test

---
+++ test/puzzle/js/app.js
@@ -0,0 +1,17 @@
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const image4 = document.getElementById('image4');
+const image5 = document.getElementById('image5');
+const image6 = document.getElementById('image6');
+const image7 = document.getElementById('image7');
+const image8 = document.getElementById('image8');
+const image9 = document.getElementById('image9');
+const images = [image1, image2, image3, image4, image5, image6, image7, image8, image9];
+let temp = [];
+let compteur = 0;
+function changeImage() {
+    compteur++;
+    temp.push();
+
+}

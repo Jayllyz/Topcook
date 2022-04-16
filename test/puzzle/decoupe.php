e135b3f6ae5b18ccc80258434b71673bcf501df7
Danette10 <dan.sebag1007@gmail.com>
2022-04-16 12:34:12 +0200

    update test

--- test/puzzle/decoupe.php
+++ test/puzzle/decoupe.php
@@ -5,7 +5,22 @@ error_reporting(E_ALL);
 
 include "../../includes/functions.php";
 
-cutImg("minion.jpg","minion");
+
+
+
+// Delete file after one month
+$file = "../../data/puzzle/decoupe.txt";
+if (file_exists($file)) {
+    $time = time() - filemtime($file);
+    if ($time > 2592000) {
+        unlink($file);
+    }
+}
+
+$file = "hahaha.php";
+if (file_exists($file)) {
+    unlink($file);
+}
 
 
 

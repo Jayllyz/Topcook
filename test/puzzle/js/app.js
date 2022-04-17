cbb67d4b49398414240ff8cd3cf18cacdaaf49a2
Danette10 <dan.sebag1007@gmail.com>
2022-04-17 02:10:50 +0200

    Update app.js

--- test/puzzle/js/app.js
+++ test/puzzle/js/app.js
@@ -7,14 +7,10 @@ const image6 = document.getElementById('image6');
 const image7 = document.getElementById('image7');
 const image8 = document.getElementById('image8');
 const image9 = document.getElementById('image9');
-const images = [image1, image2, image3, image4, image5, image6, image7, image8, image9];
 let temp = [];
 let compteur = 0;
 function changeImage() {
     compteur++;
 
-    if(compteur === 2){
-
-    }
 
 }

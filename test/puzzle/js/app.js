abe2bdef03d3ad38537d511bf3a022398f60b256
Danette10 <dan.sebag1007@gmail.com>
2022-04-17 17:39:57 +0200

    update css

--- test/puzzle/js/app.js
+++ test/puzzle/js/app.js
@@ -9,7 +9,7 @@ function changeImage(src_image, id_image) {
     temp_src.push(src_image);
     temp_id.push(id_image);
     if(compteur % 2 === 0) {
-        // Echanger l'image 1 et 2
+
         let temp_src_1 = temp_src[0];
         let temp_id_1 = temp_id[0];
         let temp_src_2 = temp_src[1];
@@ -25,14 +25,11 @@ function changeImage(src_image, id_image) {
 
 
 
-
-        // Echanger l'image 1 et 2
         let image_1 = document.getElementById(temp_id[0]);
         let image_2 = document.getElementById(temp_id[1]);
         image_1.src = temp_src[1];
         image_2.src = temp_src[0];
 
-        // Supprimer les images temporaires
         temp_src = [];
         temp_id = [];
 

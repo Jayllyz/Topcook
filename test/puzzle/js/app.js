80465a356bb466a9dbad58dbcbc20e6aa4dadb72
Antony David <antonydavid945@gmail.com>
2022-04-20 15:58:42 +0200

    Update app.js

--- test/puzzle/js/app.js
+++ test/puzzle/js/app.js
@@ -4,26 +4,17 @@ let result_images = [];
 let verify_images = [];
 let compteur = 0;
 
-
-function randomImg(){
-    let tabid = [];
-    let img1 = document.getElementById("1").id;
-    let img2 = document.getElementById("2").id;
-    let img3 = document.getElementById("3").id;
-    let img4 = document.getElementById("4").id;
-    let img5 = document.getElementById("5").id;
-    let img6 = document.getElementById("6").id;
-    let img7 = document.getElementById("7").id;
-    let img8 = document.getElementById("8").id;
-    let img9 = document.getElementById("9").id;
-    for(let i = 1; i <= 9; i++){
-        tabid.push(i);
-    }
-    tabid.sort(() => Math.random() - 0.5);
-    console.log(tabid);
-    for(let i = 1; i <= 9; i++){
-        img + i = tabid[i];
-    }
+function randomImg() {
+  let tabid = [];
+  for (let i = 1; i <= 9; i++) {
+    tabid.push(i);
+  }
+  tabid.sort(() => Math.random() - 0.5);
+  console.log(tabid);
+  for (let i = 1; i <= 9; i++) {
+    let img = document.getElementById(i).id;
+    img = tabid[i];
+  }
 }
 
 /*    let children = document.getElementById("puzzle").children;
@@ -35,40 +26,31 @@ function randomImg(){
     }
     */
 
-
-
-
 function changeImage(src_image, id_image) {
-    compteur++;
+  compteur++;
 
-    temp_src.push(src_image);
-    temp_id.push(id_image);
-    if(compteur % 2 === 0) {
+  temp_src.push(src_image);
+  temp_id.push(id_image);
+  if (compteur % 2 === 0) {
+    let temp_src_1 = temp_src[0];
+    let temp_id_1 = temp_id[0];
+    let temp_src_2 = temp_src[1];
+    let temp_id_2 = temp_id[1];
 
-        let temp_src_1 = temp_src[0];
-        let temp_id_1 = temp_id[0];
-        let temp_src_2 = temp_src[1];
-        let temp_id_2 = temp_id[1];
+    temp_src[0] = temp_src_2;
+    temp_id[0] = temp_id_2;
+    temp_src[1] = temp_src_1;
+    temp_id[1] = temp_id_1;
 
-        temp_src[0] = temp_src_2;
-        temp_id[0] = temp_id_2;
-        temp_src[1] = temp_src_1;
-        temp_id[1] = temp_id_1;
+    result_images.push(id_image);
+    verify_images.push(temp_id[0]);
 
-        result_images.push(id_image);
-        verify_images.push(temp_id[0]);
+    let image_1 = document.getElementById(temp_id[0]);
+    let image_2 = document.getElementById(temp_id[1]);
+    image_1.src = temp_src[1];
+    image_2.src = temp_src[0];
 
-
-
-        let image_1 = document.getElementById(temp_id[0]);
-        let image_2 = document.getElementById(temp_id[1]);
-        image_1.src = temp_src[1];
-        image_2.src = temp_src[0];
-
-        temp_src = [];
-        temp_id = [];
-
-    }
-
-
+    temp_src = [];
+    temp_id = [];
+  }
 }

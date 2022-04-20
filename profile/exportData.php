d9b0dbf390bd33d54c26e71ef9db7b527942e037
Antony David <antonydavid945@gmail.com>
2022-04-20 22:22:48 +0200

    Update exportData.php

--- profile/exportData.php
+++ profile/exportData.php
@@ -3,31 +3,38 @@ session_start();
 ini_set("display_errors", 1);
 ini_set("display_startup_errors", 1);
 error_reporting(E_ALL);
-include '../includes/db.php';
+include "../includes/db.php";
 
-$query = $db->query("SELECT id, pseudo, email, date_birth, creation FROM USER WHERE id = ".$_SESSION['id']);
+$query = $db->query(
+  "SELECT id, pseudo, email, date_birth, creation FROM USER WHERE id = " .
+    $_SESSION["id"]
+);
 
-$delimiter = ",";
-$filename = "user-data_" . date('Y-m-d') . ".csv";
+$filename = "user-data_" . date("Y-m-d") . ".csv";
 
-$f = fopen('php://memory', 'w');
+$f = fopen("php://data", "w");
 
+$fields = ["ID", "PSEUDO", "EMAIL", "DATE_NAISSANCE", "DATE_INSCRIPTION"];
+fputcsv($f, $fields);
 
-$fields = array('ID', 'PSEUDO', 'EMAIL', 'DATE_NAISSANCE', 'DATE_INSCRIPTION');
-fputcsv($f, $fields, $delimiter);
-
-while($row = $query->fetch(PDO::FETCH_ASSOC)){
-    $lineData = array($row['id'], $row['pseudo'], $row['email'], $row['date_birth'], $row['creation']);
-    fputcsv($f, $lineData, $delimiter);
+while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+  $lineData = [
+    $row["id"],
+    $row["pseudo"],
+    $row["email"],
+    $row["date_birth"],
+    $row["creation"],
+  ];
+  fputcsv($f, $lineData);
 }
 
 fseek($f, 0);
 
-header('Content-Type: text/csv');
+header("Content-Type: text/csv; charset=utf-8");
 header('Content-Disposition: attachment; filename="' . $filename . '";');
 
 fpassthru($f);
 
-exit;
+exit();
 
 ?>

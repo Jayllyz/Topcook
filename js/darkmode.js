a952b8fdce7f9fbfb772498d33b1d1a6b7874edf
Danette10 <dan.sebag1007@gmail.com>
2022-04-21 15:01:59 +0200

    update darkmode

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,12 +1,20 @@
 let darkmode = localStorage.getItem("darkmode");
 const darkmodeToggle = document.querySelector("#darkmode");
+const darkmodeLogo = document.getElementById("darkmode");
+const logo_admin = document.getElementById("logo_admin");
 const enableDarkMode = () => {
     document.body.classList.add('darkmode');
     localStorage.setItem("darkmode", "enabled");
+    darkmodeLogo.style.transform = "rotate(180deg)";
+    darkmodeLogo.style.transition = "0.5s";
+    logo_admin.style.fill = "white";
 };
 const disableDarkMode = () => {
     document.body.classList.remove('darkmode');
     localStorage.setItem("darkmode", null);
+    darkmodeLogo.style.transform = "rotate(0deg)";
+    darkmodeLogo.style.transition = "0.5s";
+    logo_admin.style.fill = "black";
 };
 
 if (darkmode === "enabled") {
@@ -16,6 +24,7 @@ darkmodeToggle.addEventListener("click", () => {
     darkmode = localStorage.getItem("darkmode");
     if (darkmode !== "enabled") {
         enableDarkMode();
+
     } else {
         disableDarkMode();
     }

1db869dad5155f9fa5af12721f1fe7483030ebe5
Danette10 <dan.sebag1007@gmail.com>
2022-04-22 02:43:05 +0200

    update style

--- js/captcha.js
+++ js/captcha.js
@@ -24,7 +24,6 @@ function randomImg() {
         j++;
         img.id = idImg;
     }
-
     for (let i = 1; i <= 9; i++) {
         img = document.getElementById(i);
         let srcImg = img.src;
@@ -32,7 +31,6 @@ function randomImg() {
         j++;
         img.src = "images/captcha/minion/" + img.id + ".jpg";
     }
-
 }
 function verifCaptcha(){
     let count = 0;
@@ -50,28 +48,22 @@ function verifCaptcha(){
         return true;
     }
 }
-
 function changeImage(src_image, id_image) {
     compteurForCaptcha++;
-
     temp_src.push(src_image);
     temp_id.push(id_image);
     document.getElementById(id_image).style.border = "2px solid #17760A";
-
     if (compteurForCaptcha % 2 === 0) {
         let temp_src_1 = temp_src[0];
         let temp_id_1 = temp_id[0];
         let temp_src_2 = temp_src[1];
         let temp_id_2 = temp_id[1];
-
         temp_src[0] = temp_src_2;
         temp_id[0] = temp_id_2;
         temp_src[1] = temp_src_1;
         temp_id[1] = temp_id_1;
-
         result_images.push(id_image);
         verify_images.push(temp_id[0]);
-
         let image_1 = document.getElementById(temp_id[0]);
         let image_2 = document.getElementById(temp_id[1]);
         image_1.src = temp_src[1];
@@ -82,13 +74,11 @@ function changeImage(src_image, id_image) {
         document.getElementById(temp_id[1]).style.border = "none";
         temp_src = [];
         temp_id = [];
-
         if(verifCaptcha() === true){
             submit.style.display = "block";
             p.innerHTML = "Validé";
             captchaBtn.style.display = "none";
             validateCaptcha.innerHTML = "<p class='text-success'>Captcha validé <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Feedbin-Icon-check.svg/1280px-Feedbin-Icon-check.svg.png' width='30' alt='validate'></p>";
-
         }
     }
 }

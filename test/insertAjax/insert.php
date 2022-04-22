806f6d984b12615f8aef2372da5abd52cab5b3af
Antony David <antonydavid945@gmail.com>
2022-04-22 14:41:34 +0200

    up

--- test/insertAjax/insert.php
+++ test/insertAjax/insert.php
@@ -1,6 +1,11 @@
 <?php
+session_start();
+include "../../includes/db.php";
+$test = $_POST["msg"];
 
-
-
+$sql = $db->query("INSERT INTO TEST (message) VALUES (:test)");
+$sql->execute([
+  "test" => $test,
+]);
 
 ?>
--- test/insertAjax/js/app.js
+++ test/insertAjax/js/app.js
@@ -1,11 +1,17 @@
-function insert(){
-    const xhttp = new XMLHttpRequest();
-    const msg = document.getElementById("message").value;
-    xhttp.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
+function insert() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.open(
+    "POST",
+    "https://topcook.site/test/insertAjax/insert.php?msg=",
+    true
+  );
+  request.setRequestHeader("Content-Type", "text/plain");
+  const msg = document.getElementById("message").value;
+  xhttp.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      console.log("test");
+    }
+  };
 
-        }
-    };
-    xhttp.open("POST", "insert.php", true);
-    xhttp.send("msg="+msg);
+  xhttp.send0("msg" + msg);
 }

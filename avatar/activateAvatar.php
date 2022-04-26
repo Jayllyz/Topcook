51127a3a5681abc552acc19dc98402fcce934a16
Danette10 <dan.sebag1007@gmail.com>
2022-04-26 21:39:56 +0200

    Update activateAvatar.php

--- avatar/activateAvatar.php
+++ avatar/activateAvatar.php
@@ -5,9 +5,12 @@ ini_set("display_errors", 1);
 ini_set("display_startup_errors", 1);
 error_reporting(E_ALL);
 
-$sql = $db->query("SELECT avatar FROM USER WHERE id = " . $_SESSION["id"]);
+$sql = $db->prepare("SELECT avatar FROM USER WHERE id = :id");
+$sql->execute([
+    'id' => $_SESSION['id']
+]);
 $selectResult = $sql->fetch(PDO::FETCH_ASSOC);
-if ($sql === "0") {
+if ($selectResult['avatar'] === "0") {
   $update = $db->query(
     "UPDATE USER SET avatar = '1' WHERE id = " . $_SESSION["id"]
   );

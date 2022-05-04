9902f5cfcbf01a189d88ea39249d70b19dc76ea8
Jayllyz <antonydavid945@gmail.com>
2022-05-04 13:28:07 +0200

    fix forms.js

--- js/form.js
+++ js/form.js
@@ -4,12 +4,13 @@ function validateForm(nameForm) {
   let y = document.forms[nameForm].elements;
   console.log(y);
   for (i = 0; i < y.length; i++) {
-    x = document.forms[nameForm].elements[i].value;
-    console.log(x);
-    if (x === "" && x.attributes.input) {
+    x = document.forms[nameForm].elements[i];
+    console.log(x.nodeName);
+    if (x.value === "" && x.nodeName === "INPUT") {
+      console.log("empty");
+      console.log(x.nodeName);
       alert("Veuillez remplir tous les champs");
       return false;
     }
   }
-  return true;
 }

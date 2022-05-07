6c5471767745dafd39f2ab327b5ae08fd76d4307
Antony David <antonydavid945@gmail.com>
2022-05-07 14:24:38 +0200

    export

--- profile/exportData.php
+++ profile/exportData.php
@@ -4,10 +4,10 @@ require('../includes/fpdf/fpdf.php');
 include "../includes/db.php";
 $query = $db->query(
     "SELECT id, pseudo, email, date_birth, creation FROM USER WHERE id = " .
-    $_SESSION["id"]
+        $_SESSION["id"]
 );
 $user = $query->fetchAll(PDO::FETCH_ASSOC);
-foreach ($user as $infoUser){
+foreach ($user as $infoUser) {
     $id = $infoUser["id"];
     $pseudo = $infoUser["pseudo"];
     $email = $infoUser["email"];
@@ -16,21 +16,47 @@ foreach ($user as $infoUser){
     $date_birth = date("d/m/Y", strtotime($date_birth));
     $creation = $infoUser["creation"];
 
-$pdf = new FPDF();
-$pdf->AddPage();
-$pdf->SetFont('Arial','B',16);
-$pdf->Cell(80);
-$pdf->Image('https://topcook.site/images/logo_topcook.png',10,6,30);
-$pdf->Cell(40,30,'Vos informations');
-$pdf->Ln();
-$pdf->Cell(40,10,'Votre pseudo : '.$pseudo);
-$pdf->Ln();
-$pdf->Cell(40,10,'Votre email : '.$email);
-$pdf->Ln();
-$pdf->Cell(40,10,'Votre date de naissance : '.$date_birth);
-$pdf->Ln();
-$pdf->Cell(40,10,'Votre date d\'inscription : '.$creation);
-// Download the file in the desktop
-$pdf->Output('' . $pseudo . '_topcook.pdf','D');
+    $pdf = new FPDF();
+    $pdf->AddPage();
+    $pdf->SetFont('Arial', 'B', 16);
+    $pdf->Cell(80);
+    $pdf->Image('https://topcook.site/images/logo_topcook.png', 10, 6, 30);
+    $pdf->Cell(40, 30, 'Vos informations');
+    $pdf->Ln();
+    $pdf->Cell(40, 10, 'Votre pseudo : ' . $pseudo);
+    $pdf->Ln();
+    $pdf->Cell(40, 10, 'Votre email : ' . $email);
+    $pdf->Ln();
+    $pdf->Cell(40, 10, 'Votre date de naissance : ' . $date_birth);
+    $pdf->Ln();
+    $pdf->Cell(40, 10, 'Votre date d\'inscription : ' . $creation);
+    $pdf->Ln();
+    // Download the file in the desktop
 
 }
+$query = $db->query(
+    "SELECT message,date FROM FORUM_MSG  WHERE id_user = " .
+        $_SESSION["id"]
+);
+$userMsg = $query->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($userMsg as $infoUser) {
+    $message = $infoUser["message"];
+    $date = $infoUser["date"];
+    $pdf->Cell(40, 10, 'Message : ' . $message . ' - Date : ' . $date);
+    $pdf->Ln();
+}
+$query = $db->query(
+    "SELECT message,date_send FROM COMMENTAIRE  WHERE id_user = " .
+        $_SESSION["id"]
+);
+$userCom = $query->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($userCom as $infoUser) {
+    $message = $infoUser["message"];
+    $date = $infoUser["date_send"];
+    $pdf->MultiCell(40, 10, 'Commentaire : ' . $message . ' - Date : ' . $date, 2);
+    $pdf->Ln();
+}
+
+$pdf->Output('' . $pseudo . '_topcook.pdf', 'D');

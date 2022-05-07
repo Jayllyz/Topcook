20ffdc58ae360bc3a7aa9e5556f6918dca00ea3e
Danette10 <dan.sebag1007@gmail.com>
2022-05-07 18:04:35 +0200

    up

--- includes/resolution.php
+++ includes/resolution.php
@@ -1,5 +1,6 @@
 <?php
 // redimensionner l'image
+if($ext === "jpeg" || $ext === "jpg"){
 $image = imagecreatefromjpeg($destination);
 $width = imagesx($image);
 $height = imagesy($image);
@@ -24,3 +25,27 @@ imagecopyresampled(
 imagejpeg($new_image, $destination);
 imagedestroy($new_image);
 imagedestroy($image);
+}elseif($ext === "png"){
+$image = imagecreatefrompng($destination);
+$width = imagesx($image);
+$height = imagesy($image);
+    $new_width = 400;
+    $new_height = 400;
+
+    $new_image = imagecreatetruecolor($new_width, $new_height);
+    imagecopyresampled(
+        $new_image,
+        $image,
+        0,
+        0,
+        0,
+        0,
+        $new_width,
+        $new_height,
+        $width,
+        $height
+    );
+    imagepng($new_image, $destination);
+    imagedestroy($new_image);
+    imagedestroy($image);
+}

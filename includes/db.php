d9dc4abb2724dcf46592071bc0c1028d5802a440
Danette10 <38246813+Danette10@users.noreply.github.com>
2022-12-09 15:28:22 +0100

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
 <?php
-$username = 'topcook';
-$password = 'TopCook.2022#ESGI';
+$username = 'username';
+$password = 'password';
 try {
-  $db = new PDO('mysql:host=164.132.229.157:3306;dbname=topcook', $username, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+  $db = new PDO('mysql:host=localhost;dbname=dbname', $username, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 } catch (PDOException $e) {
   echo "Erreur : " . $e->getMessage();
 }
